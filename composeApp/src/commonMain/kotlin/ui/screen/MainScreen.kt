83205e1e13e3464577404e529cc8fb1abfd608f7
Arnau Mora <arnyminerz@proton.me>
2024-04-11 20:14:49 +0200

    Started main ui sketch
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/commonMain/kotlin/ui/screen/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/screen/MainScreen.kt
@@ -3,10 +3,22 @@ package ui.screen
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.core.screen.Screen
+import io.github.alexzhirkevich.cupertino.adaptive.AdaptiveScaffold
+import io.github.alexzhirkevich.cupertino.adaptive.AdaptiveTopAppBar
+import io.github.alexzhirkevich.cupertino.adaptive.ExperimentalAdaptiveApi
 
+@OptIn(ExperimentalAdaptiveApi::class)
 class MainScreen : Screen {
     @Composable
     override fun Content() {
-        Text("Main Screen")
+        AdaptiveScaffold(
+            topBar = {
+                AdaptiveTopAppBar(
+                    title = { Text("Main Screen") }
+                )
+            }
+        ) { paddingValues ->
+
+        }
     }
 }
